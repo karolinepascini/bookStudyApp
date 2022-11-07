63801201d8de17672ef4370b04b88c9efb2d6b7f
Karoline Pascini <karoline.pascini@etec.sp.gov.br>
2022-11-07 11:42:05 -0300

    0711_novembro

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,19 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
 })
-export class Tab4Page implements OnInit {
-  activeTab: string = "chats"
-  constructor() { }
+export class Tab4Page{
+ activeTab;
 
+  constructor() { }
   segmentChange(e){
     this.activeTab = e.target.value;
   }
-
-  ngOnInit() {
-  }
-
 }
