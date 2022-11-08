db9031411d603e46d1b6bab49fc98bf5e4112de1
Karoline Pascini <61022117+karolinepascini@users.noreply.github.com>
2022-11-08 08:59:44 -0300

    quase...

--- src/app/matbio/rnaedna/rnaedna-routing.module.ts
+++ src/app/matbio/rnaedna/rnaedna-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { RnaednaPage } from './rnaedna.page';
+import { BioPage } from '../../bio/bio.page';
 
 const routes: Routes = [
   {
     path: '',
     component: RnaednaPage
+  },
+  {
+    path: 'bio',
+    component: BioPage
   }
 ];
 
