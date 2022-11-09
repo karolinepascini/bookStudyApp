158d2a35fb0a637057867a365180458e81b2abdf
Karoline Pascini <61022117+karolinepascini@users.noreply.github.com>
2022-11-09 12:28:12 -0300

    oi

--- src/app/matbio/icp/icp-routing.module.ts
+++ src/app/matbio/icp/icp-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IcpPage } from './icp.page';
+import { BioPage } from '../../materia/bio/bio.page';
 
 const routes: Routes = [
   {
     path: '',
     component: IcpPage
+  },
+  {
+    path: 'bio',
+    component: BioPage
   }
 ];
 
